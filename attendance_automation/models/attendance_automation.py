40eb8eb8d81f9a24a5ae9ec5c2592a833449e08e
UBtrNvME <mooniron.k@gmail.com>
2020-06-19 11:43:37 +0600

    [TEST] Checking work on the server and fixing part 13

--- attendance_automation/models/attendance_automation.py
+++ attendance_automation/models/attendance_automation.py
@@ -40,8 +40,8 @@ class AttendanceAutomation(models.Model):
         try_to_access_zone = self.env["acs.zone"].browse(reader.to_zone_id)
         _logger.warn(employee)
         _logger.warn(try_to_access_zone)
-        _logger.warn(try_to_access_zone != None)
-        if try_to_access_zone != []:
+        _logger.warn(try_to_access_zone != {})
+        if try_to_access_zone != {}:
             if employee.job_id not in try_to_access_zone.permitted_roles.ids:
                 return (employee.name, try_to_access_zone.name)
 
