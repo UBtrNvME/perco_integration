372154f9816149bfac81948cdc64d6843784fa84
UBtrNvME <mooniron.k@gmail.com>
2020-06-26 11:08:51 +0600

    [ADD] Added late employee view

--- attendance_automation/models/hr_employee.py
+++ attendance_automation/models/hr_employee.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from datetime import datetime
+
+from odoo import api, fields, models
 
 
 class Employee(models.Model):
@@ -9,3 +11,34 @@ class Employee(models.Model):
         column1="employee",
         column2="zone",
         relation="employee_working_place_rel")
+
+    @api.model
+    def sort_employee_by(self):
+        print("im here")
+        date_now = fields.Date.today()
+        print(date_now)
+        time_to_attend = datetime(year=date_now.year, month=date_now.month, day=date_now.day, hour=3, minute=0,
+                                  second=0)
+        time_today = datetime(year=date_now.year, month=date_now.month, day=date_now.day - 1, hour=18, minute=0,
+                              second=0)
+        late_employee_ids = []
+        print(time_to_attend, late_employee_ids)
+        employees = self.env["hr.employee"].search([])
+        print(employees.ids)
+        for employee in employees.ids:
+            print(employee)
+            todays_first_employee_attendance_before_time_to_attend = self.env["hr.attendance"].search(
+                [["id", "=", employee], ["check_in", "<=", time_to_attend], ["check_in", ">", time_today]], limit=1, order="check_in")
+            print(todays_first_employee_attendance_before_time_to_attend)
+            if not todays_first_employee_attendance_before_time_to_attend:
+                late_employee_ids.append(employee)
+                print(late_employee_ids)
+
+        return {
+            'type'     : 'ir.actions.act_window',
+            'name'     : "Late Employees",
+            'view_mode': "tree,kanban,form",
+            'res_model': 'hr.employee',
+            'domain'   : [["id", "in", late_employee_ids]],
+            'target'   : "current"
+        }
